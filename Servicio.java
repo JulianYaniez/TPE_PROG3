45efb6c8228086e1405d9be69bb5f2ce6ecd4797
Julian Fidel Yañez <julianfidel06@gmail.com>
2026-06-09 18:08:32 -0300

    Complegidad computacional del servicio uno actualizada a O(1). Preguntar servicio 2.

--- Servicio.java
+++ Servicio.java
@@ -1,18 +1,17 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class Servicio {
-    private List<Paquete> paquetes;
+    private Map<String, Paquete> paquetes;
     private List<Camion> camiones;
 /*
 * Expresar la complejidad temporal del constructor.
     Complejidad = O(n^2)
 */
     public Servicio(String pathCamion, String pathPaquete){
-        this.paquetes = new LinkedList<>();
+        this.paquetes = new HashMap<>();
         this.camiones = new LinkedList<>();
        createPath(pathCamion, "camion");
        createPath(pathPaquete, "paquete");
@@ -34,8 +33,10 @@ public class Servicio {
                     Camion c = new Camion(Integer.parseInt(datos[0]), datos[1], Boolean.parseBoolean(datos[2]), Integer.parseInt(datos[3]));
                     this.camiones.add(c);
                 } else {
+                    if(Integer.parseInt(datos[3]) == 1) {
                     Paquete p = new Paquete(Integer.parseInt(datos[0]), datos[1], Double.parseDouble(datos[2]), Integer.parseInt(datos[3]), Integer.parseInt(datos[4]));
-                    this.paquetes.add(p);
+                    this.paquetes.put(datos[1], p);
+                    }
                 }
             }
             
@@ -46,24 +47,21 @@ public class Servicio {
 
     /*
     * Expresar la complejidad temporal del servicio 1.
-        Complejidad = O(n)
+        Complejidad = O(1)
     */
     public Paquete servicio1(String codigoPaquete) { 
-        for(Paquete p : paquetes) {
-            if (codigoPaquete != null && codigoPaquete.equals(p.getCodigo())) {
-                return p;
-            }
-        }
-        return null;
+        return paquetes.get(codigoPaquete);
     }
 
     /*
     * Expresar la complejidad temporal del servicio 2.
-        Complejidad = O(n)
+        Complejidad = O(1)
     */
     public List<Paquete> servicio2(boolean contieneAlimentos) {
         List<Paquete> res = new LinkedList<>();
-        for(Paquete p : paquetes) {
+        Iterator<Paquete> paquetes2 = this.paquetes.values().iterator();
+        while(paquetes2.hasNext()) {
+            Paquete p = paquetes2.next();
             if(contieneAlimentos == (p.getContiene_alimentos() == 1 ? true : false) ) {
                 res.add(p);
             }
@@ -78,8 +76,9 @@ public class Servicio {
         Complejidad = O(n)
     */
     public List<Paquete> servicio3(int urgenciaMinima, int urgenciaMaxima) {
-        List<Paquete> res = new LinkedList<>();
-        for(Paquete p : paquetes) {
+        List<Paquete> res = new LinkedList<>();Iterator<Paquete> paquetes2 = this.paquetes.values().iterator();
+        while(paquetes2.hasNext()) {
+            Paquete p = paquetes2.next(); 
             if(urgenciaMinima <= p.getLvl_urgencia() && p.getLvl_urgencia() <= urgenciaMaxima) {
                 res.add(p);
             }
