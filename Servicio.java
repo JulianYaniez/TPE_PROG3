4c10d7df0d069039aef3c6a991232b23dd0e314f
MarcosTami <marquitostamim@gmail.com>
2026-06-16 17:53:21 -0300

    viva peron

--- Servicio.java
+++ Servicio.java
@@ -1,10 +1,14 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class Servicio {
-    private Map<String, Paquete> paquetes;
+    private Map<Boolean, Map<String, Paquete>> paquetes;
+    // private Map<String, Paquete> paquetes;
+    //private ArryList() conAlimentos;
+    //private ArryList() sinAliemntos
     private List<Camion> camiones;
 /*
 * Expresar la complejidad temporal del constructor.
@@ -12,6 +16,12 @@ public class Servicio {
 */
     public Servicio(String pathCamion, String pathPaquete){
         this.paquetes = new HashMap<>();
+        this.paquetes.put(true,  new HashMap<>());  // Para alimentos
+        this.paquetes.put(false,  new HashMap<>()); // Para no alimentos
+
+        //this.conAlimentos = new ArrayList();
+        //this.sinAlimentos = new ArrayList()
+
         this.camiones = new LinkedList<>();
        createPath(pathCamion, "camion");
        createPath(pathPaquete, "paquete");
@@ -33,10 +43,14 @@ public class Servicio {
                     Camion c = new Camion(Integer.parseInt(datos[0]), datos[1], Boolean.parseBoolean(datos[2]), Integer.parseInt(datos[3]));
                     this.camiones.add(c);
                 } else {
-                    if(Integer.parseInt(datos[3]) == 1) {
                     Paquete p = new Paquete(Integer.parseInt(datos[0]), datos[1], Double.parseDouble(datos[2]), Integer.parseInt(datos[3]), Integer.parseInt(datos[4]));
-                    this.paquetes.put(datos[1], p);
-                    }
+                    this.paquetes.get(Integer.parseInt(datos[3]) == 1 ? true : false).put(datos[1], p);
+                    //this.paquetes.put(datos[1], p)
+                    //if(datos[3]) == 1){
+                    //conComida.add(p);
+                    //else{
+                    //sinComida.add(p)
+                    //}
                 }
             }
             
@@ -50,7 +64,14 @@ public class Servicio {
         Complejidad = O(1)
     */
     public Paquete servicio1(String codigoPaquete) { 
-        return paquetes.get(codigoPaquete);
+        Paquete p = null;
+        p = paquetes.get(true).get(codigoPaquete);
+        if (p==null) {
+            p = paquetes.get(false).get(codigoPaquete);
+        }
+
+        return p;
+        //return this.paquetes.get(codigoPaquete)
     }
 
     /*
@@ -58,15 +79,8 @@ public class Servicio {
         Complejidad = O(1)
     */
     public List<Paquete> servicio2(boolean contieneAlimentos) {
-        List<Paquete> res = new LinkedList<>();
-        Iterator<Paquete> paquetes2 = this.paquetes.values().iterator();
-        while(paquetes2.hasNext()) {
-            Paquete p = paquetes2.next();
-            if(contieneAlimentos == (p.getContiene_alimentos() == 1 ? true : false) ) {
-                res.add(p);
-            }
-        }
-        return res;
+        return new ArrayList<Paquete>(this.paquetes.get(contieneAlimentos).values());
+        //return new ArrayList<Paquete>(contieneAlimentos? this.conAlimentos:this.sinAliemntos)
     }
 
 
@@ -76,7 +90,9 @@ public class Servicio {
         Complejidad = O(n)
     */
     public List<Paquete> servicio3(int urgenciaMinima, int urgenciaMaxima) {
-        List<Paquete> res = new LinkedList<>();Iterator<Paquete> paquetes2 = this.paquetes.values().iterator();
+        List<Paquete> res = new LinkedList<>();
+        // Iterator<Paquete> paquetes2 = this.paquetes.values().iterator();
+        Iterator<Paquete> it1 = this.paquetes. 
         while(paquetes2.hasNext()) {
             Paquete p = paquetes2.next(); 
             if(urgenciaMinima <= p.getLvl_urgencia() && p.getLvl_urgencia() <= urgenciaMaxima) {
@@ -85,4 +101,75 @@ public class Servicio {
         }
         return res;
     }
+    private HashMap<Camion, ArrayList<Paquete>> solucion;
+
+
+    private double minimoPesoSinAsignar; 
+    
+    public int asignarPaquetes(){
+        HashMap<Camion, ArrayList<Paquete>>asignado = new HashMap<>(); // paquetes asiganos
+        HashMap<Camion, Double> carga = new HashMap<>(); // peso actual de cada camion
+
+        asignarClaves(asignado); 
+
+        backtraking(asignado, carga ,new ArrayList<Paquete>(paquetes.values()), 0, pesoSinAsigActual());
+        
+        return 0;
+    }
+
+    private double pesoSinAsigActual() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'pesoSinAsigActual'");
+    }
+
+    private void asignarClaves(HashMap<Camion, ArrayList<Paquete>> asignado) {
+        Iterator<Camion> it = camiones.iterator();
+        while (it.hasNext()) {
+           asignado.put(it.next(), new ArrayList<>());//o linkedList
+        } 
+    }
+
+
+    private void backtraking(HashMap<Camion, ArrayList<Paquete>>asignado,  
+        HashMap<Camion, Double> carga,ArrayList<Paquete> pack, int index, double pesoActSinAsig) {
+            
+        if (pack.size()-1 == index && (this.minimoPesoSinAsignar>pesoActSinAsig) || pesoActSinAsig == 0) {
+
+            Iterator<Camion> it =asignado.keySet().iterator();
+            while (it.hasNext()) {
+                Camion camion=it.next();
+                solucion.put(camion, new ArrayList<>(asignado.get(camion)));
+            }
+            return;
+
+        }
+
+
+        Iterator<Camion> it =asignado.keySet().iterator();
+
+        Paquete paquete = pack.get(index);
+
+        while (it.hasNext()) {
+            Camion camion = it.next();
+            double cargaCamion = carga.get(camion);
+
+            if(camion.getCapacidad()>(cargaCamion + paquete.getPeso()) && 
+            (paquete.getContiene_alimentos() == 1? true:false) == camion.getEsta_refrigerado() ){
+                Double cargaNueva = paquete.getPeso();//obtiene el peso actual del paquete
+
+                carga.put(camion, cargaCamion+cargaNueva);//asigana el peso actual 
+               asignado.get(camion).add(paquete);//asigna el paquete a un camion
+                pesoActSinAsig-=paquete.getPeso();
+
+                backtraking(asignado, carga, pack, index+1, pesoActSinAsig);
+
+                pesoActSinAsig+=paquete.getPeso();
+               asignado.get(camion).remove(paquete);// desasigan del paquete
+                carga.put(camion, cargaCamion); // elimina el peso 
+            }
+        
+        }
+    }
+
+
 }
